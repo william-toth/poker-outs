80b028344ba5b3a6d4df9ecf52884a0017e4da9a
william-toth <wat4th@gmail.com>
2021-04-15 14:39:09 -0400

    everything

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+// set to 'production' or 'development' in your env
+
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
+
+const autoprefixer = require('autoprefixer');
+const postcssPresets = require('postcss-preset-env');
+
+module.exports = {
+  devServer: {
+    hot: true,
+  },
+  mode: env,
+  entry: ['./src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ //this is the part you want to add
+            { loader: 'babel-loader'}
+        ]
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            ident: 'postcss',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  autoprefixer(),
+                  postcssPresets({ browsers: 'last 2 versions' }),
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }), 
+    new ESLintPlugin({}),
+    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+    }),
+  ], 
+};
